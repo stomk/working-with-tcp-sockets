5ce1ff8b3dc3a30f5800e72430082b37a3b7b79e
Shingo Tomioka <s.tomioka.103@gmail.com>
2021-10-16 17:43:11 +0900

    [Chapter 18] Add common module

---
+++ code/ftp/common.rb
@@ -0,0 +1,15 @@
+module FTP
+  module Common
+    CRLF = "\r\n"
+
+    def initialize(port)
+      @control_socket = TCPServer.new(port)
+      trap(:INT) { exit }
+    end
+
+    def respond(response)
+      @client.write(response)
+      @client.write(CRLF)
+    end
+  end
+end
