27a564fde838174531883411b298203cfcbd3091
Shingo Tomioka <s.tomioka.103@gmail.com>
2021-10-16 18:56:33 +0900

    Fix: Add outer loop

--- code/ftp/arch/process_per_connection.rb
+++ code/ftp/arch/process_per_connection.rb
@@ -6,24 +6,26 @@ module FTP
     include Common
 
     def run
-      @client = @control_socket.accept
+      loop do
+        @client = @control_socket.accept
 
-      pid = fork do
-        respond "220 OHAI"
+        pid = fork do
+          respond "220 OHAI"
 
-        handler = CommandHandler.new(self)
+          handler = CommandHandler.new(self)
 
-        loop do
-          if (request = @client.gets(CRLF))
-            respond handler.handle(request)
-          else
-            @client.close
-            break
+          loop do
+            if (request = @client.gets(CRLF))
+              respond handler.handle(request)
+            else
+              @client.close
+              break
+            end
           end
         end
-      end
 
-      Process.detach(pid)
+        Process.detach(pid)
+      end
     end
 
   end
--- code/ftp/arch/serial.rb
+++ code/ftp/arch/serial.rb
@@ -6,17 +6,19 @@ module FTP
     include Common
 
     def run
-      @client = @control_socket.accept
-      respond "220 OHAI"
-
-      handler = CommandHandler.new(self)
-
       loop do
-        if (request = @client.gets(CRLF))
-          respond handler.handle(request)
-        else
-          @client.close
-          break
+        @client = @control_socket.accept
+        respond "220 OHAI"
+
+        handler = CommandHandler.new(self)
+
+        loop do
+          if (request = @client.gets(CRLF))
+            respond handler.handle(request)
+          else
+            @client.close
+            break
+          end
         end
       end
     end
